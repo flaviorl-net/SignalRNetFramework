d2f6cffb437157f8babc071920058764c150689a
Flavio Ribeiro Lima <flavioweb1@hotmail.com>
2021-04-28 22:28:55 -0300

    Initial commit

---
+++ SignalRNetFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRNetFramework.MvcApplication" Language="C#" %>
---
+++ SignalRNetFramework/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading;
+
+namespace SignalRNetFramework.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void SystemMessage()
+        {
+            string[] messages = new string[5] { "Testing...", "1", "2", "3", "Finished" };
+
+            foreach (var item in messages)
+            {
+                Thread.Sleep(1000);
+                Clients.Caller.addNewMessageToPage("Adm", item);
+            }
+        }
+
+    }
+}
